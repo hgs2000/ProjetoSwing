78376fada835858d18654b7e231cafed3c57f648
miguel <>
2017-07-05 11:49:31 +0000

    bug do login corrigido

--- JavaApplication17/src/Objetos/Funcionario.java
+++ JavaApplication17/src/Objetos/Funcionario.java
@@ -11,8 +11,16 @@ public class Funcionario {
     }
 
     public boolean validLogin(String username, char[] password) {
+        String senha = "";
+        String senhaBD = "";
+        for (int i = 0; i < password.length; i++) {
+            senha = senha + password[i];
+        }
+        for (int i = 0; i < login_pessoa.getPassword().length; i++) {
+            senhaBD = senhaBD + login_pessoa.getPassword()[i];
+        }
         return ((login_pessoa.getUserName().equals(username))
-                && (login_pessoa.getPassword() == password));
+                && (senha.equals(senhaBD)));
 
     }
 }
